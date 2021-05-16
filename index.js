fd96a34b1dbbd1a970e695f73eec8142f91bba04
CyberFlaw <53346567+CyberFlaw@users.noreply.github.com>
2021-05-16 20:18:14 +0530

    patches due to dumb mistakes

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
 const main = require("./api/server");
-const config = require("./config.json");
 
 const launchServer = (port) => {
   main(port);
 };
 
-// Still Under Development
-// const configure = (obj) => {
-//   for (const [key, value] of Object.entries(config.env)) {
-//     console.log(`${key}: ${value}`);
-//   }
-// };
-
 module.exports = {
   launchServer,
-  //   configure,
 };
